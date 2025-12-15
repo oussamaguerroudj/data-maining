5a104027ce8fc956cea07a6fba6075199ef93bc2
oussamaguerroudj <oussama.guerroudj@ensia.edu.dz>
2025-12-15 03:24:25 +0100

    fixing frontend problem

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    css: {
-        postcss: {
-            plugins: [
-
-                require('@tailwindcss/postcss'),
-                require('autoprefixer'),
-            ],
-        },
-    },
-})
+    server: {
+        port: 3000,
+        open: true
+    }
+});
